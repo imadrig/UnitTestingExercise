c306106da5843f3e6003e9a95b33d1409d081f20
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-07-17 12:44:47 -0400

    Initial Commit and completed part of exercise

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -7,33 +7,48 @@ namespace UnitTestingExercise.Tests
     {
         [Theory]
         [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(7, 3, 40, 50)]
+        [InlineData(3, 3, 3, 9)]
+        [InlineData(7, 3, 1, 11)]
+        [InlineData(-1, -2, -2, -5)]
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
 
             //Arrange
-            // create a Calculator object
-            
+            // create a Calculator object - DONE
+            Calculator calculator1 = new Calculator();
+
 
             //Act
-                // call the Add method that is located in the Calculator class
-                // and store its result in a variable named actual
+            // call the Add method that is located in the Calculator class
+            // and store its result in a variable named actual - DONE
+            var actual1 = calculator1.Add(num1, num2, num3);
+
 
             //Assert
-                //Assert.Equal(expected, actual);
+            //Assert.Equal(expected, actual); - DONE
+            Assert.Equal(expected, actual1); 
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(7, 5, 2)]//Add test data <-------
+        [InlineData(2, 3, -1)]
+        [InlineData(17, 7, 10)]
+        [InlineData(-9, -11, 2)]
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
 
-            //Arrange
+            //Arrange - DONE
+            Calculator calculator = new Calculator();
 
-            //Act
+            //Act - DONE
+            var actual = calculator.Subtract(minuend, subtrhend);
 
-            //Assert
+
+            //Assert - DONE
+            Assert.Equal(expected, actual);
 
         }
 
--- UnitTestingExercise/Calculator.cs
+++ UnitTestingExercise/Calculator.cs
@@ -3,14 +3,14 @@ namespace UnitTestingExercise
 {
     public class Calculator
     {
-        // Step 1: Add a reference to your UnitTestingExercise project:
+        // Step 1: Add a reference to your UnitTestingExercise project: DONE
 
         // Right-click on the 'Dependencies' folder in your UnitTestingExercise.Tests project
         // and add your reference to the UnitTestingExercise project
 
 
 
-        // Step 2: Create an Add method that accepts 3 integers, adds those integers, and returns an integer
+        // Step 2: Create an Add method that accepts 3 integers, adds those integers, and returns an integer - DONE
         //For Example:
         public int Add(int num1, int num2, int num3)
         {
@@ -18,12 +18,17 @@ namespace UnitTestingExercise
         }
 
 
-        // Step 3: Navigate to the UnitTests.cs file and complete the AddTest unit test method 
+        // Step 3: Navigate to the UnitTests.cs file and complete the AddTest unit test method  - DONE
 
 
         // Step 4: 
         // Create a Subtract method that accepts 2 integers
-        // Keep track of which number is getting passed as minuend and subtrahend
+        // Keep track of which number is getting passed as minuend and subtrahend  - DONE
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+
 
 
         // Step 5: Navigate to the UnitTests.cs file and complete the SubtractTest unit test method
